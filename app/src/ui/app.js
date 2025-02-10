0bccb2024afe02257da5abb19791270b142682a2
miguel <migueldc.dev@protonmail.com>
2025-02-10 15:16:16 +0100

    add: styles and emoji menu

--- app/src/ui/app.js
+++ app/src/ui/app.js
@@ -1,18 +1,26 @@
+const { fromEvent } = require("rxjs");
+const { map } = require("rxjs/operators")
 const socket = io();
+
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (input.value) {
-    socket.emit('chat message', input.value);
+const formSubmit$ = fromEvent(form, 'submit').pipe(
+  map(event => {
+    event.preventDefault();
+    const value = input.value;
     input.value = '';
-  }
-});
+    return value;
+  })
+)
+formSubmit$.subscribe(value => {
+  socket.emit('chat message', value);  
+})
 
 socket.on('chat message', (msg) => {
-  let message = document.createElement('li');
+  const message = document.createElement('div');
+  message.classList.add("message")
   message.textContent = msg;
   messages.appendChild(message);
 })
