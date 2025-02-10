0cdb880e3ae30dcc2f29b89e4675fce1438ff052
miguel <migueldc.dev@protonmail.com>
2025-02-10 10:01:13 +0100

    add: socket connection

--- app/index.ts
+++ app/index.ts
@@ -2,14 +2,23 @@ import express from 'express';
 import { createServer } from 'http';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { Server } from 'socket.io'
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public/index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('a user disconnected');
+    })    
 });
 
 server.listen(3000, () => {
