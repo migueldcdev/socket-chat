622c90acd079c48ae53f424ada792068ba73e9d7
miguel <migueldc.dev@protonmail.com>
2025-02-10 09:44:08 +0100

    add: create server

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,17 @@
-const { Server } = require("socket.io")
+import express from 'express';
+import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const app = express();
+const server = createServer(app);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'public/index.html'));
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
