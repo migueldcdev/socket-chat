a00b500cd64f0d72807394687e19085af6ccc564
miguel <migueldc.dev@protonmail.com>
2025-02-12 15:32:02 +0100

    do: uninstall rxjs

--- app/src/ui/app.js
+++ app/src/ui/app.js
@@ -1,21 +1,14 @@
-const { fromEvent } = require("rxjs");
-const { map } = require("rxjs/operators")
 const socket = io();
 
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
 
-const formSubmit$ = fromEvent(form, 'submit').pipe(
-  map(event => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     const value = input.value;
     input.value = '';
-    return value;
-  })
-)
-formSubmit$.subscribe(value => {
-  socket.emit('chat message', value);  
+    socket.emit('chat message', value);
 })
 
 socket.on('chat message', (msg) => {
